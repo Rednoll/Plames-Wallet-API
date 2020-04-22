f1f60a9180622de66d0d9e8391d59b8ec178bfff
Redn_oll <rednoll12@gmail.com>
2020-04-22 15:50:33 +0300

    dev

--- src/main/java/com/inwaiders/plames/modules/wallet/domain/wallet/Wallet.java
+++ src/main/java/com/inwaiders/plames/modules/wallet/domain/wallet/Wallet.java
@@ -2,7 +2,7 @@ package com.inwaiders.plames.modules.wallet.domain.wallet;
 
 import java.util.Collection;
 
-import com.inwaiders.plames.modules.wallet.domain.account.CurrencyAccount;
+import com.inwaiders.plames.modules.wallet.domain.account.PrivateCurrencyAccount;
 import com.inwaiders.plames.modules.wallet.domain.currency.Currency;
 
 import enterprises.inwaiders.plames.api.user.User;
@@ -12,10 +12,10 @@ public interface Wallet {
 	public void setOwner(User user);
 	public User getOwner();
 
-	public void addPrivateAccount(CurrencyAccount account);
-	public CurrencyAccount getPrivateAccount(Currency currency);
+	public void addPrivateAccount(PrivateCurrencyAccount account);
+	public PrivateCurrencyAccount getPrivateAccount(Currency currency);
 
-	public Collection<CurrencyAccount> getPrivateAccounts();
+	public Collection<PrivateCurrencyAccount> getPrivateAccounts();
 	
 	public void save();
 	public void delete();
